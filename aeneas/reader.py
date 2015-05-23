b4d4b65326df53d050a7405f36986b590b1d3f44
Daniel Standage <daniel.standage@gmail.com>
2015-05-23 16:29:57 -0400

    Minimal reader implementation, untested

---
+++ aeneas/reader.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+#
+# -----------------------------------------------------------------------------
+# Copyright (C) 2015 Daniel Standage <daniel.standage@gmail.com>
+#
+# This file is part of aeneas (http://github.com/standage/aeneas) and is
+# licensed under the ISC license: see LICENSE.txt.
+# -----------------------------------------------------------------------------
+
+import sys
+from .comment import Comment
+from .directive import Directive
+from .feature import Feature
+
+
+class GFF3Reader():
+    """Loads sequence features and other GFF3 entries into memory."""
+
+    def __init__(self, instream=sys.stdin, infilename=None,
+                 assumesorted=False):
+        assert not instream or not infilename, ('provide either an instream '
+                                                'or an infile name, not both')
+        self.instream = instream
+        if infilename:
+            self.infilename = infilename
+            self.instream = open(infilename, 'r')
+        self.assumesorted = assumesorted
+
+    def __iter__(self):
+        """Generator function returns GFF3 entries."""
+        self._reset()
+
+        for line in self.instream:
+            line = line.rstrip()
+            if line == '':
+                continue
+            elif line == '###' and self.assumesorted:
+                for obj in self._resolve_features():
+                    yield obj
+            elif line.startswith('#'):
+                if line.startswith('##') and line[2].isalpha():
+                    record = Directive(line)
+                else:
+                    record = Comment(line)
+                self.records.append(record)
+            else:
+                feature = Feature(line)
+
+                parentid = feature.get_attribute('Parent')
+                if parentid is None:
+                    self.records.append(feature)
+                else:
+                    if parentid not in self.featsbyparent:
+                        self.featsbyparent[parentid] = list()
+                    self.featsbyparent[parentid].append(feature)
+
+                featureid = feature.get_attribute('ID')
+                if featureid is None:
+                    if featureid in self.featsbyid:
+                        # Validate multi-features
+                        other = self.featsbyid[featureid]
+                        assert feature.type == other.type, \
+                            'feature type disagreement for ID="%s": %s vs %s' \
+                            % (featureid, feature.type, other.type)
+                        assert feature.seqid == other.seqid, \
+                            'feature seq disagreement for ID="%s": %s vs %s' \
+                            % (featureid, feature.seqid, other.seqid)
+                        other.add_sibling(feature)
+                    else:
+                        self.featsbyid[featureid] = feature
+
+        for obj in self._resolve_features():
+            yield obj
+
+    def _resolve_features(self):
+        """Resolve Parent/ID relationships and yield all top-level features."""
+
+        for parentid in self.featsbyparent:
+            parent = self.featsbyid[parentid]
+            for child in self.featsbyparent[parentid]:
+                parent.add_child(child)
+                if child.is_multi:
+                    for sibling in child.siblings:
+                        parent.add_child(sibling)
+
+        for feature in sorted(self.toplevelfeatures):
+            yield feature
+        self._reset()
+
+    def _reset(self):
+        """Clear internal data structure."""
+        self.records = list()
+        self.featsbyid = dict()
+        self.featsbyparent = dict()
+        self.countsbytype = dict()
