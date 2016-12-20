58c15aa7a8596381dca61f1905e869004dd265ca
Daniel Standage <daniel.standage@gmail.com>
2016-12-19 20:06:39 -0800

    Document CLI

--- tag/__main__.py
+++ tag/__main__.py
@@ -18,8 +18,17 @@ mains = {
 
 
 def main(args=None):
+    """
+    Entry point for the tag CLI.
+
+    Isolated as a method so that the CLI can be called by other Python code
+    (e.g. for testing), in which case the arguments are passed to the function.
+    If no arguments are passed to the function, parse them from the command
+    line.
+    """
     if args is None:
         args = tag.cli.parser().parse_args()
+
     assert args.cmd in mains
     mainmethod = mains[args.cmd]
     mainmethod(args)
