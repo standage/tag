7021f1504d27378078cbb5369f7d82e6aa81f08b
Daniel Standage <daniel.standage@gmail.com>
2017-02-08 16:16:06 -0800

    Test and file for exon dedup [skip ci]

---
+++ tests/test_gene.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# -----------------------------------------------------------------------------
+# Copyright (C) 2017 Daniel Standage <daniel.standage@gmail.com>
+#
+# This file is part of tag (http://github.com/standage/tag) and is licensed
+# under the BSD 3-clause license: see LICENSE.
+# -----------------------------------------------------------------------------
+
+import pytest
+import tag
+
+
+def test_exon_dedup():
+    output = StringIO()
+    reader = tag.GFF3Reader(tag.pkgdata('turt-iso.gff3'))
+    dedup = tag.gene.exon_dedup(reader)
+    exons = [e for e in tag.select.features(dedup, type='exon')]
+    assert len(exons) == 7
