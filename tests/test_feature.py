179419ff3c2bc21fbfb74d9f6acc79a41a791baa
Daniel Standage <daniel.standage@gmail.com>
2017-04-18 14:17:53 -0700

    Typo

--- tests/test_feature.py
+++ tests/test_feature.py
@@ -59,7 +59,7 @@ def test_basic():
     with pytest.raises(TypeError) as te:
         f0 = Feature()
     assert 'missing 1 required positional argument' in str(te) or \
-        'takes exactly 2 arguements' in str(te)
+        'takes exactly 2 arguments' in str(te)
 
     f0 = Feature(None)
     assert f0.seqid is None
